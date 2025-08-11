7eae47b0acd55cc4823a010ae18da0dca8ea42ed
Evgen Lysenko <evgen.lysenko@gmail.com>
2025-08-11 11:13:46 +0000

    tests - serial - navigator.serial

--- src/state/ducks/balance/sagas.ts
+++ src/state/ducks/balance/sagas.ts
@@ -1,13 +1,50 @@
-import { all, fork, put, select, takeEvery } from "redux-saga/effects";
+import { all, call, fork, put, select, takeEvery } from "redux-saga/effects";
 import { PayloadAction, TypeConstant } from "typesafe-actions";
 import { IApplicationState } from "..";
 import { BalanceActionTypes, IBalanceState } from "./types";
 import { balanceStarted } from "./actions";
 import ErrorUtils from "../../../utils/ErrorUtils";
+//import { SerialPort } from 'serialport'
+
+//const serialport = new SerialPort({ path: '/dev/ttyS0', baudRate: 9600 });
+
+const makeCallPorts = async (): Promise<any> => {
+    try {
+        if ("serial" in navigator) {
+            const ports = await (navigator.serial as any).getPorts();
+            console.log(ports);
+            return ports;
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    }
+}
+
+const makeCallPort = async (): Promise<any> => {
+    try {
+        if ("serial" in navigator) {
+            const port = await (navigator.serial as any).requestPort();
+            console.log(port);
+            return port;
+        }
+    }
+    catch (err) {
+        console.error(err);
+        return null;
+    }
+}
 
 function* handleBalanceStart(action: PayloadAction<TypeConstant, IBalanceState>): Generator {
     try {
         const { connected } = (yield select((state: IApplicationState) => state.balance)) as IBalanceState;
+
+        const res = yield call(makeCallPort);
+        //const res = SerialPort.list();
+        console.log(res);
+            // The Web Serial API is supported.
+        //}
 
         yield put(balanceStarted(!connected));
     }
