dede98fa185c0dd65f4e1c2ea871ccd524d7483c
Evgen Lysenko <evgen.lysenko@gmail.com>
2025-08-31 13:04:23 +0300

    parse refactoring

---
+++ src/balancer/CsvParser.ts
@@ -0,0 +1,68 @@
+import { Balancer } from "./Balancer";
+
+export class CsvParser
+{
+    startPos: number = 0;
+    hasNext: boolean = true;
+    buf: Uint8Array = new Uint8Array(0);
+    size: number = 0;
+
+    public parseStart(buf: Uint8Array, size: number, startPos: number) {
+        this.buf = buf;
+        this.size = size;
+        this.startPos = startPos;
+        this.hasNext = true;
+    }
+
+    public parseNumber(defaultValue: number): number {
+        if (!this.hasNext)
+            return defaultValue;
+
+        if (this.startPos >= this.size) {
+            this.hasNext = false;
+            return defaultValue;
+        }
+
+        let i = this.startPos;
+        let value = 0;
+        let minus = false;
+        for (; i < this.size; ++i) {
+            const ch = this.buf[i];
+            if (ch === 0x2C) { // ,
+                this.hasNext = true;
+                this.startPos = i + 1;
+                break;
+            }
+
+            if (ch >= 0x30 && ch <= 0x39) { // 0 - 9
+                value *= 10;
+                value += ch - 0x30;
+            }
+            else if (ch === 0x2D) { // -
+                minus = true;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (minus)
+            value = -value;
+        //console.log(rpm);
+
+        for (; i < this.size; ++i) {
+            const ch = this.buf[i];
+            if (ch === 0x2C) { // ,
+                this.hasNext = true;
+                this.startPos = i + 1;
+                return value;
+            }
+        }
+
+        if (i >= this.size) {
+            this.hasNext = false;
+        }
+
+        return value;
+    }
+}
